0e01f41bc72e5d374857f83211e64710b1bb7e0c
Pandelis Zembashis <p@ndel.is>
2025-05-16 13:42:02 +0100

    chore: fix

--- symlink.sh
+++ symlink.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+# Check if the .config directory exists, and if not, create it.
+if [ ! -d "$HOME/.config" ]; then
+  mkdir -p $HOME/.config
+fi
+
+# Check if the .ghostty directory exists, and if not, create it.
+if [ ! -d "$HOME/.config/ghostty" ]; then
+  mkdir -p $HOME/.config/ghostty
+fi
 
 ln -sf $(pwd)/wezterm/init.lua $HOME/.wezterm.lua
 ln -sf $(pwd)/aerospace/aerospace.toml $HOME/.aerospace.toml
