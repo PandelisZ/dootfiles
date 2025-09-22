cd8631f1b35a10d19bc7a543613a86ee2600c782
Pandelis Zembashis <p@ndel.is>
2025-09-22 10:57:53 +0100

    chore: fix links

--- symlink.sh
+++ symlink.sh
@@ -10,9 +10,15 @@ if [ ! -d "$HOME/.config/ghostty" ]; then
 fi
 
 ln -sf $(pwd)/wezterm/init.lua $HOME/.wezterm.lua
-ln -sf $(pwd)/aerospace/aerospace.toml $HOME/.aerospace.toml
 ln -sf $(pwd)/ghostty/config $HOME/.config/ghostty/config
 ln -sf $(pwd)/nvim $HOME/.config/nvim
 ln -sf $(pwd)/tmux/tmux.conf $HOME/.tmux.conf
 ln -sf $(pwd)/gitconfig/dotgitconfig $HOME/.gitconfig
-ln -sf $(pwd)/hypr $HOME/.config/hypr
+# check if on linux and if so symlink the hypr config
+if [ "$(uname)" == "Linux" ]; then
+  ln -sf $(pwd)/hypr $HOME/.config/hypr
+fi
+
+if [ "$(uname)" == "Darwin" ]; then
+  ln -sf $(pwd)/aerospace/aerospace.toml $HOME/.aerospace.toml
+fi
